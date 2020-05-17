3d0ea5f6eaa11d9f259cb801f06c93b2f472434a
codingindepth <bpandey1011@gmail.com>
2020-05-17 11:57:18 -0400

    1 file modified

--- components/common/Header.js
+++ components/common/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  const activeStyle = { color: "#F15B2A" };
+  const activeStyle = { color: "#8b0000" };
   return (
     <nav>
       <NavLink to="/" activeStyle={activeStyle} exact>
