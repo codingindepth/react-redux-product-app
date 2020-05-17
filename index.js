c993c626585a3613ec6a82a8bcbb279cffafc448
codingindepth <bpandey1011@gmail.com>
2020-05-17 11:59:41 -0400

    1 file modified, 2 added, 2 deleted

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
-import HomePage from './components/pages/HomePage';
-import AboutPage from './components/pages/AboutPage';
-import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import HomePage from './components/common/HomePage';
+import AboutPage from './components/common/AboutPage';
 import Header from './components/common/Header';
+
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
 import ProductForm from './components/pages/ProductForm';
 import configureStore from './components/redux/configureStore';
 import { Provider as ReduxProvider } from "react-redux";
