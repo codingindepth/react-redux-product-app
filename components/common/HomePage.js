e61595518c6b8e4c3da0b9af89e2b0b0787d83b3
codingindepth <bpandey1011@gmail.com>
2020-05-22 10:19:17 -0400

    1 file modified

--- components/common/HomePage.js
+++ components/common/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => (
-  <div className="jumbotron">
+  <div>
     <h1>Product App</h1>
     <p>This product app is made to demo
     <a target="_blank" href="https://medium.com/@codingindepth"> Coding In Depth</a> stories.
